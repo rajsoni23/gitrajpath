9accf25222900c2733c00dd72a4957d0928ad1c9
Raj Soni <137527742+rajsoni23@users.noreply.github.com>
2026-08-09 09:14:36 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,10 +3,10 @@ export const testCatalogue = {
     name: 'Erythrocyte Sedimentation Rate (ESR)',
     price: 100,
     params: [
-      { name: 'ESR', unit: 'mm/hr', range: '[M: 0-15] [F: 0-20]', defaultValue: '10' }
-      { name: '', unit: '', range: '', defaultValue: '' }
-      { name: '', unit: '', range: '', defaultValue: '' }
-      { name: '', unit: '', range: '', defaultValue: '' }
+      { name: 'ESR', unit: 'mm/hr', range: '[M: 0-15] [F: 0-20]', defaultValue: '10' },
+      { name: '', unit: '', range: '', defaultValue: '' },
+      { name: '', unit: '', range: '', defaultValue: '' },
+      { name: '', unit: '', range: '', defaultValue: '' },
       { name: 'ESR', unit: 'mm/hr', range: '[M: 0-15] [F: 0-20]', defaultValue: '10' }
       
     ]
